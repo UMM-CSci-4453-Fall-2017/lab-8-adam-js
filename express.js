03fe09c013e5e64ae684d4f864fcb1c31ffb0a37
casey369 <casey369@morris.umn.edu>
2017-11-07 18:50:30 -0600

    still working - progress

--- express.js
+++ express.js
@@ -22,17 +22,66 @@ app.get("/buttons",function(req,res){
      res.send(rows);
   }})(res));
 });
+
 app.get("/click",function(req,res){
-  var id = req.param('id');
-  var sql = 'YOUR SQL HERE'
-  console.log("Attempting sql ->"+sql+"<-");
+  // buttonID
+  var btnID = req.param('id');
+  var itemInfo = null;
+  var itemInfoSql = 'select invID, label, price from institutional_casey.current_transaction where buttonID = ' + btnID;
 
-  connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an insertion error:");
-             console.log(err);}
-     res.send(err); // Let the upstream guy know how it went
-  }})(res));
+  queryPromiser(DBF, itemInfoSql)
+  .then(function (idResult) {
+    itemInfo = idResult[0];
+    var validSql = 'select exists (select invID from institutional_casey.current_transaction where invID = ' + itemInfo.invID + ') as isValid';
+    return queryPromiser(DBF, validSql);
+  })
+  .then(function (existResult) {
+    if (existResult[0].isValid) {
+      // Increase amount by 1
+      var updateSql = 'UPDATE institutional_casey.current_transaction SET amount = amount + 1 WHERE invID = ' + itemInfo.invID;
+      return queryPromiser(DBF, updateSql);
+    } else {
+      // Create a new one
+      var insertSql = 'insert into institutional_casey.current_transaction values (' + itemInfo.invID + ',' + 1 + ',\"' + itemInfo.label + '\",' + itemInfo.price + ')';
+      return queryPromiser(DBF, insertSql);
+    }
+  })
+  .then(function (dummy) {
+    var showSql = 'select * from institutional_casey.current_transaction';
+    return queryPromiser(DBF, showSql);
+  })
+  .then(function (currentTransaction) {
+    res.send(currentTransaction);
+  })
+  .catch(function(err){console.log("DANGER:",err)});
 });
-// Your other API handlers go here!
 
+
+
+//app.get("/click",function(req,res){
+ // var id = req.param('id');
+ // var sql = ''
+  //console.log("Attempting sql ->"+sql+"<-");
+
+  //connection.query(sql,(function(res){return function(err,rows,fields){
+     //if(err){console.log("We have an insertion error:");
+      //       console.log(err);}
+    // res.send(err); // Let the upstream guy know how it went
+  //}})(res));
+//});
+
+// Your other API handlers go here!
+// app.get("/changeUser",function(req,res){
+//	var sql = "select * from institutional_casey.users"	
+ // connection.query(sql,(function(res){return function(err,rows,fields){	  
+    // if(err){console.log("We have an error:");
+  //           console.log(err);}
+//	  res.send(rows);
+  // }})(res));
+// });
+// app.get("/delete", function(req,res){
+//	var ID = req.param('id');
+//	var sql = 'Delete from institutional_casey.current_transaction where ID =' + ID;
+
+//}
 app.listen(port);
--- public/click.js
+++ public/click.js
@@ -1,54 +1,67 @@
 angular.module('buttons',[])
-  .controller('buttonCtrl',ButtonCtrl)
-  .factory('buttonApi',buttonApi)
-  .constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
+	.controller('buttonCtrl',ButtonCtrl)
+	.factory('buttonApi',buttonApi)
+	.constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
 
 function ButtonCtrl($scope,buttonApi){
-   $scope.buttons=[]; //Initially all was still
-   $scope.errorMessage='';
-   $scope.isLoading=isLoading;
-   $scope.refreshButtons=refreshButtons;
-   $scope.buttonClick=buttonClick;
+	$scope.buttons=[]; //Initially all was still
+	$scope.errorMessage='';
+	$scope.isLoading=isLoading;
+	$scope.refreshButtons=refreshButtons;
+	$scope.buttonClick=buttonClick;
+	$scope.currenT=[];
+	$scope.total= 0;
 
-   var loading = false;
 
-   function isLoading(){
-    return loading;
-   }
-  function refreshButtons(){
-    loading=true;
-    $scope.errorMessage='';
-    buttonApi.getButtons()
-      .success(function(data){
-         $scope.buttons=data;
-         loading=false;
-      })
-      .error(function () {
-          $scope.errorMessage="Unable to load Buttons:  Database request failed";
-          loading=false;
-      });
- }
-  function buttonClick($event){
-     $scope.errorMessage='';
-     buttonApi.clickButton($event.target.id)
-        .success(function(){})
-        .error(function(){$scope.errorMessage="Unable click";});
-  }
-  refreshButtons();  //make sure the buttons are loaded
+	var loading = false;
 
+	function isLoading(){
+		return loading;
+	}
+	function refreshButtons(){
+		loading=true;
+		$scope.errorMessage='';
+		buttonApi.getButtons()
+			.success(function(data){
+				$scope.buttons=data;
+				loading=false;
+			})
+			.error(function () {
+				$scope.errorMessage="Unable to load Buttons:  Database request failed";
+				loading=false;
+			});
+	}
+	function buttonClick($event){
+		$scope.errorMessage='';
+		buttonApi.clickButton($event.target.id)
+			.success(function(currenT){
+			$scope.currenT = currenT;
+			$scope.total = TotalAmount(curenT)
+			})
+			.error(function(){$scope.errorMessage="Unable click";});
+	}
+	refreshButtons();  //make sure the buttons are loaded
+
+}
+function changeUser(username, password){
+	$scope.username = username;
+	$scope.password = password;
+	buttonApi.changeUser(username,password);
+	.success(function(){})
+		.error(function(){$scope.errorMessage="Unable to change user";});
 }
 
 function buttonApi($http,apiUrl){
-  return{
-    getButtons: function(){
-      var url = apiUrl + '/buttons';
-      return $http.get(url);
-    },
-    clickButton: function(id){
-      var url = apiUrl+'/click?id='+id;
-//      console.log("Attempting with "+url);
-      return $http.get(url); // Easy enough to do this way
-    }
- };
+	return{
+		getButtons: function(){
+			var url = apiUrl + '/buttons';
+			return $http.get(url);
+		},
+		clickButton: function(id){
+			var url = apiUrl+'/click?id='+id;
+			//      console.log("Attempting with "+url);
+			return $http.get(url); // Easy enough to do this way
+		}
+	};
 }
 
